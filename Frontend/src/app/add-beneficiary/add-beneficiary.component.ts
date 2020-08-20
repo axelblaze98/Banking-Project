1cfb96d7568a127c6efe37addaf7677082de7ade
yash-manchanda <yashm99999@gmail.com>
2020-08-20 22:23:53 +0530

    functionality added in beneficiary component

--- Frontend/src/app/add-beneficiary/add-beneficiary.component.ts
+++ Frontend/src/app/add-beneficiary/add-beneficiary.component.ts
@@ -12,6 +12,33 @@ export class AddBeneficiaryComponent implements OnInit {
   ngOnInit(): void {
   }
   titles=['Mr.', 'Mrs.' , 'Ms.'];
+  checkInfo="checked";
+  // someProperty = new Beneficiary("",null);
 
+  saveBeneficiry(){
+    let acNumber = ((document.getElementById("bAccountNumber") as HTMLInputElement).value);
+    let reAcNumber = ((document.getElementById("bReAccountNumber") as HTMLInputElement).value);
+
+    // alert(acNumber);
+    if(acNumber != reAcNumber){
+      alert("Account Number Mis-Match"); 
+    }
+    else{
+    }
+  }
+
+  validate(){
+    // let acNumber = ((document.getElementById("bAccountNumber") as HTMLInputElement).value);
+    // let reAcNumber = ((document.getElementById("bReAccountNumber") as HTMLInputElement).value);
+
+    // // alert(acNumber);
+    // if(acNumber != reAcNumber){
+    //   alert("Account Number Mis-Match"); 
+    //   this.misMatch=true;
+    // }
+    // else{
+    //   this.misMatch=false;
+    // }
+  }
 
 }
