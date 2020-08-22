2caf91040ed71b586cfe6b86e11ce8c6b2830444
isha <isha.lalchandani@gmail.com>
2020-08-23 02:09:40 +0530

    frontend working with routing

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,3 +1,12 @@
+import { AdminloginComponent } from './adminlogin/adminlogin.component';
+import { OpenAccountComponent } from './open-account/open-account.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { ForgotuseridComponent } from './forgotuserid/forgotuserid.component';
+import { ForgotpasswordComponent } from './forgotpassword/forgotpassword.component';
+import { LearnmoreComponent } from './learnmore/learnmore.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
 import { FaqmainComponent } from './faqmain/faqmain.component';
 import { AccountmainpageComponent } from './accountmainpage/accountmainpage.component';
 import { HomepagemainComponent } from './homepagemain/homepagemain.component';
@@ -5,7 +14,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-
+  {path: '', component: HomepagemainComponent},
+  {path: 'faqmain', component: FaqmainComponent},
+  {path: 'accountmainpage', component: AccountmainpageComponent},
+  {path: 'aboutus', component: AboutusComponent},
+  {path: 'contactus', component: ContactusComponent},
+  {path: 'learnmore', component: LearnmoreComponent},
+  {path: 'forgotpassword', component: ForgotpasswordComponent},
+  {path: 'forgotuserid', component: ForgotuseridComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'openaccount', component: OpenAccountComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'adminlogin', component: AdminloginComponent}
 ];
 
 @NgModule({
@@ -13,3 +33,5 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [FaqmainComponent, AccountmainpageComponent, HomepagemainComponent, AboutusComponent, ContactusComponent, LearnmoreComponent, ForgotpasswordComponent, ForgotuseridComponent, LoginComponent, RegisterComponent, OpenAccountComponent, AdminloginComponent]
+
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -2,48 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomepagemainComponent } from './homepagemain/homepagemain.component';
-import { AccountmainpageComponent } from './accountmainpage/accountmainpage.component';
-import { FaqmainComponent } from './faqmain/faqmain.component';
+
 import { AddBeneficiaryComponent } from './add-beneficiary/add-beneficiary.component';
-import { OpenAccountComponent } from './open-account/open-account.component';
-import { AdminloginComponent } from './adminlogin/adminlogin.component';
-import { ForgotpasswordComponent } from './forgotpassword/forgotpassword.component';
-import { ForgotuseridComponent } from './forgotuserid/forgotuserid.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
 import { SessionexpiredComponent } from './sessionexpired/sessionexpired.component';
 import { SetnewpasswordComponent } from './setnewpassword/setnewpassword.component';
 import { SetnewtransactionpasswordComponent } from './setnewtransactionpassword/setnewtransactionpassword.component';
 import { AccountCreationStatusComponent } from './account-creation-status/account-creation-status.component';
 import { FundTransferComponent } from './fund-transfer/fund-transfer.component';
-import { AboutusComponent } from './aboutus/aboutus.component';
-import { LearnmoreComponent } from './learnmore/learnmore.component';
-import { ContactusComponent } from './contactus/contactus.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepagemainComponent,
-    AccountmainpageComponent,
-    FaqmainComponent,
+    routingComponents,
     AddBeneficiaryComponent,
-    OpenAccountComponent,
-    AdminloginComponent,
-    ForgotpasswordComponent,
-    ForgotuseridComponent,
-    LoginComponent,
-    RegisterComponent,
     SessionexpiredComponent,
     SetnewpasswordComponent,
     SetnewtransactionpasswordComponent,
     AccountCreationStatusComponent,
     FundTransferComponent,
-    AboutusComponent,
-    LearnmoreComponent,
-    ContactusComponent
   ],
   imports: [
     BrowserModule,
