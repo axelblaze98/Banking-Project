1a99d24b17a7eb5fec2e106ba0f7b94f32b3854a
ankitrules27 <ankitrules27@gmail.com>
2020-08-23 21:04:20 +0530

    updated trackapplication logout and adminregister

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -9,6 +9,9 @@ import { SessionexpiredComponent } from './sessionexpired/sessionexpired.compone
 import { SetnewpasswordComponent } from './setnewpassword/setnewpassword.component';
 import { SetnewtransactionpasswordComponent } from './setnewtransactionpassword/setnewtransactionpassword.component';
 import { AccountCreationStatusComponent } from './account-creation-status/account-creation-status.component';
+import { LogoutComponent } from './logout/logout.component';
+import { TrackapplicationstatusComponent } from './trackapplicationstatus/trackapplicationstatus.component';
+import { AdminregisterComponent } from './adminregister/adminregister.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +20,10 @@ import { AccountCreationStatusComponent } from './account-creation-status/accoun
     SessionexpiredComponent,
     SetnewpasswordComponent,
     SetnewtransactionpasswordComponent,
-    AccountCreationStatusComponent
+    AccountCreationStatusComponent,
+    LogoutComponent,
+    TrackapplicationstatusComponent,
+    AdminregisterComponent
   ],
   imports: [
     BrowserModule,
