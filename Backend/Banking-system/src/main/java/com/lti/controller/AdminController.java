f43e718e8bfd41317fc132579bd0ac658dba387b
axelblaze98 <paarthbhandari98@gmail.com>
2020-08-24 21:20:58 +0530

    Added CORS

--- Backend/Banking-system/src/main/java/com/lti/controller/AdminController.java
+++ Backend/Banking-system/src/main/java/com/lti/controller/AdminController.java
@@ -1,6 +1,7 @@
 package com.lti.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import com.lti.status.LoginStatus;
 import com.lti.status.Status;
 import com.lti.status.Status.StatusType;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController	
 public class AdminController {
 	
--- Backend/Banking-system/src/main/java/com/lti/controller/OpenAccountController.java
+++ Backend/Banking-system/src/main/java/com/lti/controller/OpenAccountController.java
@@ -1,6 +1,7 @@
 package com.lti.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import com.lti.service.OpenAccountService;
 import com.lti.status.Status;
 import com.lti.status.Status.StatusType;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class OpenAccountController {
 	
