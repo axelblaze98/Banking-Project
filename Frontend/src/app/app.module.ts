3e529b3629fdb406011606b85d846e44f08b3e0d
isha <isha.lalchandani@gmail.com>
2020-08-24 17:53:02 +0530

    few changes

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -10,12 +10,7 @@ import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { SessionexpiredComponent } from './sessionexpired/sessionexpired.component';
-import { SetnewpasswordComponent } from './setnewpassword/setnewpassword.component';
 import { SetnewtransactionpasswordComponent } from './setnewtransactionpassword/setnewtransactionpassword.component';
-import { AccountCreationStatusComponent } from './account-creation-status/account-creation-status.component';
-import { LogoutComponent } from './logout/logout.component';
-import { TrackapplicationstatusComponent } from './trackapplicationstatus/trackapplicationstatus.component';
-import { AdminregisterComponent } from './adminregister/adminregister.component';
 @NgModule({
   declarations: [
     AppComponent,
