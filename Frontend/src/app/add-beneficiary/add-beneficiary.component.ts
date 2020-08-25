28d87871f4635048d814c8ba8de5a70cf60e052d
yash-manchanda <yashm99999@gmail.com>
2020-08-26 04:23:59 +0530

    beneficiary form validation done

--- Frontend/src/app/add-beneficiary/add-beneficiary.component.ts
+++ Frontend/src/app/add-beneficiary/add-beneficiary.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-beneficiary',
@@ -7,38 +8,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddBeneficiaryComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  error_messages = {
+    'fname': [
+      { type: 'required', message: 'Beneficiary Name is required.' },
+    ],
+    'password': [
+      { type: 'required', message: 'Account Number is required.' },
+      { type: 'required', message: 'Account Number must be of 9 digit' }
+      
+    ],
+    'confirmpassword': [
+      { type: 'required', message: 'Account Number is required.'},
+      { type: 'required', message: 'Account Number must be of 9 digit' }
+      
+    ]
+  }
+  constructor( public formBuilder: FormBuilder) { 
+    this.loginForm = this.formBuilder.group({
+      fname: new FormControl('', Validators.compose([
+        Validators.required
+      ])),
+      password: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(9)
+      
+      ])),
+      confirmpassword: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(9)
+      ])),
+    }, { 
+      validators: this.password.bind(this)
+  
+    });
+  }
 
   ngOnInit(): void {
   }
-  titles=['Mr.', 'Mrs.' , 'Ms.'];
-  checkInfo="checked";
-  // someProperty = new Beneficiary("",null);
-
-  saveBeneficiry(){
-    let acNumber = ((document.getElementById("bAccountNumber") as HTMLInputElement).value);
-    let reAcNumber = ((document.getElementById("bReAccountNumber") as HTMLInputElement).value);
-
-    // alert(acNumber);
-    if(acNumber != reAcNumber){
-      alert("Account Number Mis-Match"); 
-    }
-    else{
-    }
-  }
-
-  validate(){
-    // let acNumber = ((document.getElementById("bAccountNumber") as HTMLInputElement).value);
-    // let reAcNumber = ((document.getElementById("bReAccountNumber") as HTMLInputElement).value);
-
-    // // alert(acNumber);
-    // if(acNumber != reAcNumber){
-    //   alert("Account Number Mis-Match"); 
-    //   this.misMatch=true;
-    // }
-    // else{
-    //   this.misMatch=false;
-    // }
+  password(formGroup: FormGroup) {
+    const { value: password } = formGroup.get('password');
+    const { value: confirmPassword } = formGroup.get('confirmpassword');
+    return password === confirmPassword ? null : { passwordNotMatch: true };
   }
 
 }
