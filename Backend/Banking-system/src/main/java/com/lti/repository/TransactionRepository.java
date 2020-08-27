9fe12511a2bcf9d3a4d4c0e28f0f536b12385442
axelblaze98 <paarthbhandari98@gmail.com>
2020-08-27 18:25:34 +0530

    transactionHistory

--- Backend/Banking-system/src/main/java/com/lti/repository/TransactionRepository.java
+++ Backend/Banking-system/src/main/java/com/lti/repository/TransactionRepository.java
@@ -9,4 +9,5 @@ public interface TransactionRepository {
 	void save(Transaction transaction);
 	int getTransactionId();
 	List<Transaction> getAllRecords(String accNumber);
+	List<Transaction> getTransactionBetweenDates(String fromDate,String toDate,String accountnumber);
 }
