93b829b2b79c3d49a34951916930cf61a491e207
axelblaze98 <paarthbhandari98@gmail.com>
2020-08-27 12:36:36 +0530

    fund transfer validation

--- Frontend/src/app/fund-transfer/fund-transfer.component.ts
+++ Frontend/src/app/fund-transfer/fund-transfer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-fund-transfer',
@@ -6,10 +7,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fund-transfer.component.css']
 })
 export class FundTransferComponent implements OnInit {
-
-  constructor() { }
+  loginForm: FormGroup;
+  error_messages = {
+    'fname': [
+      { type: 'required', message: 'Account Number is required.' },
+    ],
+    
+    'lname': [
+      { type: 'required', message: 'Amount is required.' },
+    ],
+     
+    'rname': [
+      { type: 'required', message: 'Remarks required.' },
+    ]
+  }
+  constructor(public formBuilder: FormBuilder)
+   { 
+    this.loginForm = this.formBuilder.group({
+      fname: new FormControl('', Validators.compose([
+        Validators.required ,
+        Validators.minLength(9),
+        Validators.maxLength(9)
+      ])),
+      lname: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.min(1),
+        Validators.pattern(/^-?(1|[2-9]\d*)?$/)
+        
+      ])),
+      rname: new FormControl('', Validators.compose([
+        Validators.required
+      ]))
+    });
+  }
 
   ngOnInit(): void {
   }
+  Add = ['Parth', 'Isha', 'Yash', 'Ankit'];
+  types = ['NEFT', 'IMPS', 'RTGS'];
+  
 
 }
